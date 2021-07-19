498f2eaba24409fed08994eadd2d58a2430e6788
venturma <venturamatp@gmail.com>
2021-07-20 00:42:04 +0200

    correccion de errores en llamada

--- ETV/models.py
+++ ETV/models.py
@@ -14,7 +14,9 @@ def trainModel(corpusPath, nameOfModel, num_iterations = 5, _model_version = "12
     utils.checkFile(corpusPath)
 
     sess = gpt2.start_tf_sess() #config (threads and external server)
-    gpt2.finetune(sess, corpusPath, model_name=_model_version, steps=num_iterations, run_name=nameOfModel)  # steps is max number of training step
+    gpt2.finetune(sess, corpusPath,
+                  model_name=_model_version, steps=num_iterations, run_name=nameOfModel,
+                  restore_from='fresh', multi_gpu=False,)  # steps is max number of training step
 
 
 
