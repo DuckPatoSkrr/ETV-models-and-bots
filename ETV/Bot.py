469c83c83fdf3120b1e4538b04793496c418ba8f
venturma <venturamatp@gmail.com>
2021-07-19 23:53:31 +0200

    añadido el metodo toJSON y el constructor con JSON

--- ETV/Bot.py
+++ ETV/Bot.py
@@ -1,35 +1,43 @@
-#import responseGeneration
+import responseGeneration
 import models
-
+import json
 
 class BotInstance:
-
 
     #PRIVATE
 
     def _getModelBasedOnMood(self, mood):
-        #TODO
-        return self._mymodels[0]
+        #TODO puede que utilizando keywords sea una buena manera
+        return self.mymodelsnames[0]
 
 
     name = "noname"
-    _mymodels = []
     mymodelsnames = []
 
     #PUBLIC
 
-    def __init__(self, name):
+    def __init__(self, name, loadModels=[]):
         self.name = name
+        self.mymodelsnames = loadModels
 
-    def generateResponse(self, input, myMood):
+
+
+
+    def generateResponse(self, myMood, prefix=None):
         model = self._getModelBasedOnMood(myMood)
-        return responseGeneration.generateResponse(model, input)
+        return responseGeneration.generateResponse(model, prefix)
 
-    def generateText(self, myMood):
-        model = self._getModelBasedOnMood(myMood)
-        return responseGeneration.generateText(model)
 
-    def learn(self, corpus, nameOfMood):
-        #splitCorpus(corpus, train, test)
-        self._mymodels.append(models.trainModel(corpus, corpus))
+    def learn(self, corpusPath, nameOfMood):
+        models.trainModel(corpusPath,nameOfMood)
         self.mymodelsnames.append(nameOfMood)
+
+    def toJSON(self):
+        jsonFile = {"name":self.name,"mymodelsnames":self.mymodelsnames}
+        return json.dumps(jsonFile)
+
+def jsonConstructor(input):
+    out = json.loads(input)
+    name = out["name"]
+    modelsList = out["mymodelsnames"]
+    return BotInstance(name,modelsList)
