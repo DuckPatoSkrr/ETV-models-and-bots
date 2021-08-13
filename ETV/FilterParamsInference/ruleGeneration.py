38d49c62fd338954fccb6023734b360ceadebbff
jaimefd <skyjimmy7@gmail.com>
2021-08-13 20:59:08 +0200

    Creacion de hechos en facts.pl a partir de diccionario

--- ETV/FilterParamsInference/ruleGeneration.py
+++ ETV/FilterParamsInference/ruleGeneration.py
@@ -32,3 +32,47 @@ def checkSentence(pairs_dict, sentence):
             for j in range(i + 1, pamount):
                 pairs_dict[(pnouns[i], pnouns[j])].append(res.sentiment_polarity)
     return pairs_dict
+
+
+# turns a dictionary of relations into prolog facts
+def dictToFacts(text_dict):
+    dict_aux = {}
+    delete_lines = []
+    default_facts = r"./FilterParamsInference/facts.pl"
+
+    # first we read the facts file and get a list of the lines
+    r_file = open(default_facts, "r")
+    lines = r_file.readlines()
+    r_file.close()
+
+    # now we delete the lines with term relationship rules
+    # and store them in our aux dictionary
+    for i in range(0,len(lines)):
+        if lines[i].startswith("relacionTerm"):
+            line_list = lines[i].split("\"");
+            value_aux = line_list[4][1:].split(")");
+            dict_aux[(line_list[1], line_list[3])] = float(value_aux[0])
+            delete_lines.append(i)
+
+    # we make sure to delete the lines after looping through them to not mess with the loop
+    for i in range(0,len(delete_lines)):
+        del lines[i]
+
+    # now we combine this aux dictionary with the obtained from the text
+    # if the same key exists in both, we keep the newer ones, so the ones from text_dict
+    for pair in text_dict:
+        dict_aux[pair] = text_dict[pair]
+
+    # we have our final values in dict_aux, time to add them to facts.pl
+    w_file = open(default_facts, "w+")
+
+    # first, we add the old lines that we did not delete back
+    for line in lines:
+        w_file.write(line)
+
+    # now we add the new rules
+    for pair in dict_aux:
+        text_line = "relacionTerm(\"{0}\",\"{1}\",{2}).".format(pair[0], pair[1], dict_aux[pair])
+        w_file.write(text_line)
+
+    w_file.close()
