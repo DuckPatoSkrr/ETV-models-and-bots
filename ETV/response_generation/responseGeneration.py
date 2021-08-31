a39e3856ad65391a5533258fdf30217ddb5b8e0e
venturma <venturma@ucm.es>
2021-08-31 22:01:04 +0200

    comentado el pipe de slang

--- ETV/response_generation/responseGeneration.py
+++ ETV/response_generation/responseGeneration.py
@@ -148,7 +148,7 @@ def _processedText(input, nchars, positivityFactor, keywords, slangFactor):  # f
     output = _duple(input)
     output = _pipeFormat(output, nchars)
     output = _pipePositivity(output, positivityFactor)
-    output = _pipeSlang(output, slangFactor)
+    #output = _pipeSlang(output, slangFactor)
     output = _pipeKeywordCount(output, keywords)
     return _maxPoints(output)
 
